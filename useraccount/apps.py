9d81f4f895346d34a32d2901c3957ddcd66c84a2
Mmy2000 <yahianada005@gmail.com>
2024-09-23 05:17:44 +0300

    creating model for useraccount app

--- useraccount/apps.py
+++ useraccount/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UseaccountConfig(AppConfig):
+class UseraccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'useaccount'
+    name = 'useraccount'
