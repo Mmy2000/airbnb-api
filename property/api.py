db9ab30a5abab50dca15dab1b7c269208708f8e0
Mmy2000 <yahianada005@gmail.com>
2024-09-24 06:32:27 +0300

    create property

--- property/api.py
+++ property/api.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from .api import PropertyForm
 
 from rest_framework.decorators import (
     api_view,
@@ -17,3 +18,18 @@ def properties_list(request):
     properties = Property.objects.all()
     serializer = PropertiesListSerializer(properties, many=True)
     return JsonResponse({"data": serializer.data})
+
+
+@api_view(["POST", "FILES"])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({"success": True})
+    else:
+        print("error", form.errors, form.non_field_errors)
+        return JsonResponse({"errors": form.errors.as_json()}, status=400)
---
+++ useraccount/api.py
@@ -0,0 +1,11 @@
+from django.http import JsonResponse
+
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+
+from .models import User
+from .serializers import UserDetailSerializer
+
