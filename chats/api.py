41cc723f7a13f2668aac828305cfdf2a1c0bd45d
Mmy2000 <yahianada005@gmail.com>
2024-09-26 00:53:25 +0300

    starting with websockets part 3

--- chats/api.py
+++ chats/api.py
@@ -17,3 +17,21 @@ def conversations_list(request):
     serializer = ConversationListSerializer(request.user.conversations.all(), many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(["GET"])
+def conversations_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+
+    conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+    # messages_serializer = ConversationMessageSerializer(
+    #     conversation.messages.all(), many=True
+    # )
+
+    return JsonResponse(
+        {
+            "conversation": conversation_serializer.data,
+            # "messages": messages_serializer.data,
+        },
+        safe=False,
+    )
