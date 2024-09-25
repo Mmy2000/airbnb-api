91d4db6dbbd567bb00067e85d74fe518c5c2beae
Mmy2000 <yahianada005@gmail.com>
2024-09-26 02:17:16 +0300

    starting with websockets part 7

--- chats/api.py
+++ chats/api.py
@@ -35,3 +35,22 @@ def conversations_detail(request, pk):
         },
         safe=False,
     )
+
+
+@api_view(["GET"])
+def conversations_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(
+        users__in=[request.user.id]
+    )
+
+    if conversations.count() > 0:
+        conversation = conversations.first()
+
+        return JsonResponse({"success": True, "conversation_id": conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+
+        return JsonResponse({"success": True, "conversation_id": conversation.id})
